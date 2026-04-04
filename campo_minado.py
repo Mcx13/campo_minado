37ecaff1123acf67120b4601549fce130bd5c102
Mcx13 <115926546+Mcx13@users.noreply.github.com>
2026-04-04 17:47:53 -0300

    Refactor game board setup and drawing function
    Refactor drawing logic and initialize game board.

--- campo_minado.py
+++ campo_minado.py
@@ -1,18 +1,27 @@
 import pygame
 pygame.init()
 
+# Estrutura
+
+CELULA = 50
+LINHAS = 8
+COLUNAS = 8
+
 janela= pygame.display.set_mode((400,400))
 pygame.display.set_caption("Campo Minado")
 
-#Desenhar quadrados
+tabuleiro = [[0 for _ in range(COLUNAS)] for _ in range(LINHAS)]
+revelado = [[False for _ in range(COLUNAS)] for _ in range(LINHAS)]
 
-CELULA = 50
-for linha in range(8):
-    for coluna in range(8):
-        react = pygame.Rect(coluna*CELULA, linha*CELULA, CELULA, CELULA)
-        pygame.draw.react(janela, (200,200,200), rect)
+# Desenho do tabuleiro
 
-        pygame.draw.rect(janela(0,0,0), rect, 1)
+def desenhar(janela):
+        for linha in range(LINHAS):
+            for coluna in range(COLUNAS):
+                rect = pygame.Rect(coluna * CELULA, linha * CELULA, CELULA, CELULA)
+        
+                pygame.draw.rect(janela, (200,200,200), rect)
+                pygame.draw.rect(janela, (0,0,0), rect, 1)
 
 #loop
 
@@ -21,5 +30,11 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
-    
+
+    janela.fill((255,255,255))
+
+    desenhar(janela)
+
     pygame.display.flip()
+    
+pygame.quit()
